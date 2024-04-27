d4d9b88b4a307c304c2f2cc4b90ace35725e43a0
Fabito02 <138934847+Fabito02@users.noreply.github.com>
2024-04-26 22:22:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ function convert() {
     let temp = document.getElementById("temp").value
     let result = document.getElementById("result")
     let convert = 0
-    if (temp <= 0) {
-        result.innerHTML = ("Insira dados válidos")
-    }
-    else {
-        convert = (temp * 9/5) + 32
-        result.innerHTML = (`O resultado da conversão para Fahrenheit é ${convert}ºF`)
-    }
+    convert = (temp * 9/5) + 32
+    result.innerHTML = (`O resultado da conversão para Fahrenheit é ${convert}ºF`)
 }
